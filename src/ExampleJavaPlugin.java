707539bae01b8bdd41bc0b12d5a2e9456ee1dbfe
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-05-28 04:52:09 +0300

    profile, .gitignore, resources -> res

--- src/ExampleJavaPlugin.java
+++ src/ExampleJavaPlugin.java
@@ -1,15 +1,21 @@
 import FlashLauncher.base.IAccount;
+import FlashLauncher.base.IProfile;
 import FlashLauncher.plugin.Plugin;
 import FlashLauncher.plugin.PluginContext;
 
 public class ExampleJavaPlugin extends Plugin {
-
     public ExampleJavaPlugin(final PluginContext context) {
         super(context);
         addAccount(new IAccount() {
             @Override
             public String getNickname() {
                 return "ExampleAccount";
+            }
+        });
+        addProfile(new IProfile() {
+            @Override
+            public String getName() {
+                return "ExampleProfile";
             }
         });
     }
