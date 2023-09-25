5769708f96621c2817f31fcb60641b4df7e80d2f
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-09-25 04:07:27 +0300

    Update ExampleJavaPlugin.java

--- src/ExampleJavaPlugin.java
+++ src/ExampleJavaPlugin.java
@@ -1,21 +1,75 @@
+import Launcher.*;
 import Launcher.base.*;
-import Launcher.Plugin;
-import Launcher.PluginContext;
+import UIL.UI;
+import UIL.base.IImage;
 
 public class ExampleJavaPlugin extends Plugin {
-
-    public ExampleJavaPlugin(PluginContext context) {
+    public ExampleJavaPlugin(final PluginContext context) {
         super(context);
         addAccount(new IAccount() {
             @Override
-            public String getName() {
+            public String toString() {
                 return "ExampleAccount";
+            }
+
+            @Override
+            public IImage getIcon() {
+                return FlashLauncher.ICON_SETTINGS;
+            }
+
+            @Override
+            public void open(final IEditorContext context) {
+                context.add(
+                        UI.panel().size(context.width(), 48).add(
+                                UI.button(LANG_REMOVE).size(context.width() - 16, 32).pos(8, 8).onAction((s, e) -> {
+                                    removeAccount(this);
+                                    context.close();
+                                })
+                        )
+                );
+            }
+
+            @Override
+            public void preLaunch(final RunProc configuration) {
+
+            }
+
+            @Override
+            public void launch(final RunProc configuration) {
+
             }
         });
         addProfile(new IProfile() {
             @Override
-            public String getName() {
+            public String toString() {
                 return "ExampleProfile";
+            }
+
+            @Override
+            public IImage getIcon() {
+                return FlashLauncher.ICON_SETTINGS;
+            }
+
+            @Override
+            public void open(final IEditorContext context) {
+                context.add(
+                        UI.panel().size(context.width(), 48).add(
+                                UI.button(LANG_REMOVE).size(context.width() - 16, 32).pos(8, 8).onAction((s, e) -> {
+                                    removeProfile(this);
+                                    context.close();
+                                })
+                        )
+                );
+            }
+
+            @Override
+            public void preLaunch(final RunProc configuration) {
+
+            }
+
+            @Override
+            public void launch(final RunProc configuration) {
+
             }
         });
     }
