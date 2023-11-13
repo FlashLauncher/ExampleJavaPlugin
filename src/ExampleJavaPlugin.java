2d62a712db1fbac55b7d4e764b6cafafc0e72999
illa4257 <48959534+illa4257@users.noreply.github.com>
2023-11-13 05:32:40 +0200

    Update ExampleJavaPlugin.java

--- src/ExampleJavaPlugin.java
+++ src/ExampleJavaPlugin.java
@@ -6,39 +6,30 @@ import UIL.base.IImage;
 public class ExampleJavaPlugin extends Plugin {
     public ExampleJavaPlugin(final PluginContext context) {
         super(context);
-        addAccount(new IAccount() {
-            @Override
-            public String toString() {
-                return "ExampleAccount";
-            }
+        for (int i = 0; i < 1; i++)
+            addAccount(new IAccount() {
+                @Override
+                public String toString() {
+                    return "ExampleAccount";
+                }
 
-            @Override
-            public IImage getIcon() {
-                return FlashLauncher.ICON_SETTINGS;
-            }
+                @Override
+                public IImage getIcon() {
+                    return FlashLauncher.ICON_SETTINGS;
+                }
 
-            @Override
-            public void open(final IEditorContext context) {
-                context.add(
-                        UI.panel().size(context.width(), 48).add(
-                                UI.button(LANG_REMOVE).size(context.width() - 16, 32).pos(8, 8).onAction((s, e) -> {
-                                    removeAccount(this);
-                                    context.close();
-                                })
-                        )
-                );
-            }
-
-            @Override
-            public void preLaunch(final RunProc configuration) {
-
-            }
-
-            @Override
-            public void launch(final RunProc configuration) {
-
-            }
-        });
+                @Override
+                public void open(final IEditorContext context) {
+                    context.add(
+                            UI.panel().size(context.width(), 48).add(
+                                    UI.button(LANG_REMOVE).size(context.width() - 16, 32).pos(8, 8).onAction((s, e) -> {
+                                        removeAccount(this);
+                                        context.close();
+                                    })
+                            )
+                    );
+                }
+            });
         addProfile(new IProfile() {
             @Override
             public String toString() {
@@ -60,16 +51,6 @@ public class ExampleJavaPlugin extends Plugin {
                                 })
                         )
                 );
-            }
-
-            @Override
-            public void preLaunch(final RunProc configuration) {
-
-            }
-
-            @Override
-            public void launch(final RunProc configuration) {
-
             }
         });
     }
